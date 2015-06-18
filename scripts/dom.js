8f20658115573e854d2a059dac0fee835e95077b
Unknown22 <marcin700@gmail.com>
2015-06-18 20:11:11 +0200

    Zadanie 1 JavaScript wykonane

--- scripts/dom.js
+++ scripts/dom.js
@@ -6,6 +6,8 @@
         output.innerHTML += "Found element:" + elements[i].tagName + " with id: " + elements[i].id + "<br />"
         browse_elements(elements[i], output_id);
     }
+	document.getElementById('wyczysc_wyjscie').disabled = 0;
+	document.getElementById('out_1').disabled = 1;
 }
 
 function add_elements(start, output_list) {
@@ -30,4 +32,26 @@ function add_elements(start, output_list) {
         // go for recurency
         add_elements(elements[i], output_list);
     }
+	document.getElementById('wyczysc_wyjscie').disabled = 0;
+	document.getElementById('out_2').disabled = 1;
+}
+
+function wyczysc_wszystko(out_list, output){	 
+var div_out_1 = document.getElementById('out_list')
+var div_out_2 = document.getElementById('output')
+ 
+ if(div_out_1.firstChild || div_out_2.firstChild)
+	{
+		while(div_out_1.hasChildNodes())
+		{   
+			div_out_1.removeChild(div_out_1.firstChild);
+		}
+		while(div_out_2.hasChildNodes())
+		{   
+			div_out_2.removeChild(div_out_2.firstChild);
+		}
+	}    
+document.getElementById('wyczysc_wyjscie').disabled = 1;
+document.getElementById('out_1').disabled = 0;
+document.getElementById('out_2').disabled = 0;
 }
